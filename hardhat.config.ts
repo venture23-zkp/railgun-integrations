74a2a416e4911c02e7e56f7d0e35d3ad513f374d
illusioncoder7 <shubh.tiwari512@gmail.com>
2023-05-31 15:55:18 +0545

    add networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,18 @@
-
-import '@typechain/hardhat'
+// import '@typechain/hardhat'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    mumbai: {
+      url: process.env.POLYGON_MUMBAI_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    }},
   solidity: "0.8.18",
 };
